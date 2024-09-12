5a69ab14af96e228b635d9a72eb09f2fd30a4e00
JonyTask <yuusu.hyb@gmail.com>
2024-09-12 13:43:53 +0900

    feat: validation rules and messages of registraion

--- src/app/Actions/Fortify/CreateNewUser.php
+++ src/app/Actions/Fortify/CreateNewUser.php
@@ -10,8 +10,6 @@ use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class CreateNewUser implements CreatesNewUsers
 {
-    use PasswordValidationRules;
-
     /**
      * Validate and create a newly registered user.
      *
@@ -19,7 +17,7 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
-        Validator::make($input, [
+        $rules = [
             'name' => ['required', 'string', 'max:255'],
             'email' => [
                 'required',
@@ -28,8 +26,23 @@ class CreateNewUser implements CreatesNewUsers
                 'max:255',
                 Rule::unique(User::class),
             ],
-            'password' => $this->passwordRules(),
-        ])->validate();
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ];
+        $messages = [
+            'name.required' => '名前を入力してください',
+            'name.string' => '名前は文字で入力してください',
+            'name.max' => '名前は255字以下で入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.string' => 'メールアドレスは文字で入力してください',
+            'email.email' => 'メール形式で入力してください',
+            'email.max' => 'メールアドレスは255字以下で入力してください',
+            'email.unique' => 'こちらのメールアドレスはすでに登録されています',
+            'password.required' => 'パスワードを入力してください',
+            'password.string' => 'パスワードは文字列で入力してください',
+            'password.min' => 'パスワードは8文字以上で入力してください',
+            'password.confirmed' => 'パスワードの確認が一致しません'
+        ];
+        Validator::make($input, $rules, $messages)->validate();
 
         return User::create([
             'name' => $input['name'],
