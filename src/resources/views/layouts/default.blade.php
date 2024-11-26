ae27de93d2c67a0af5162eb69a5baedf7c4c1d64
JonyTask <yuusu.hyb@gmail.com>
2024-11-26 20:32:41 +0900

    change : unnecessary space default.blade.php

--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,11 +8,13 @@
     <title>@yield('title')</title>
     <script src="https://kit.fontawesome.com/42694f25bf.js" crossorigin="anonymous"></script>
     <script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
-    <link rel="stylesheet" href="{{ asset('/css/reset.css')  }}" >
-    <link rel="stylesheet" href="{{ asset('/css/common.css')  }}" >
+    <link rel="stylesheet" href="{{ asset('/css/reset.css')  }}">
+    <link rel="stylesheet" href="{{ asset('/css/common.css')  }}">
     @yield('css')
 </head>
+
 <body>
     @yield('content')
 </body>
+
 </html>
